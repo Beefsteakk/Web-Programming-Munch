c20b235fa50878cc9ef6ddd9db171887b6f42b8b
addsw <addsw@hotmail.co.uk>
2024-05-15 17:35:47 +0800

    -

---
+++ Models/PostsEntity.cs
@@ -0,0 +1,8 @@
+namespace EffectiveWebProg.Models;
+
+public class PostsEntity
+{
+    public int PostID { get; set; }
+    public string PostTitle { get; set; }
+    public string PostContent { get; set; }
+}
---
+++ Startup.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+using EffectiveWebProg.Data;
+
+public class Startup
+{
+    public IConfiguration Configuration { get; }
+
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+        // Add additional services
+    }
+}
