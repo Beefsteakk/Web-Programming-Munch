9eb0a2658ab7ab2b76daaf045c786ce887983751
addsw <addsw@hotmail.co.uk>
2024-05-18 16:43:42 +0800

    -

--- obj/Debug/net8.0/EffectiveWebProg.AssemblyInfo.cs
+++ obj/Debug/net8.0/EffectiveWebProg.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EffectiveWebProg")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c20b235fa50878cc9ef6ddd9db171887b6f42b8b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+084f8be916e5f8475668ce9ceac40a19181de344")]
 [assembly: System.Reflection.AssemblyProductAttribute("EffectiveWebProg")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EffectiveWebProg")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
