2cd0637f8f6cf9b596a348d61fae726f9d1ab6c4
BobTheVillager <chuakymarcus@gmail.com>
2024-06-03 00:42:09 +0800

    Added modal (css still required)

--- Controllers/PostsController.cs
+++ Controllers/PostsController.cs
@@ -2,6 +2,7 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using EffectiveWebProg.Models;
 using EffectiveWebProg.Data;
+using Microsoft.Extensions.ObjectPool;
 
 namespace EffectiveWebProg.Controllers;
 
@@ -35,5 +36,15 @@ public class PostsController : Controller
         _db.SaveChanges();
 
         return RedirectToAction("Posts");
+    }
+
+    [HttpPost]
+    public JsonResult GetInfo(int id)
+    {
+        List<string> ImageUrl = new List<string>();
+        ImageUrl.Add("/assets/images/photo-1556008531-57e6eefc7be4.jpeg");
+        ImageUrl.Add("/assets/images/photo-1557684387-08927d28c72a.jpeg");
+        ImageUrl.Add("/assets/images/photo-1526016650454-68a6f488910a.jpeg");
+        return Json(new {imageUrl = ImageUrl, info = $"Information for div {id}"});
     }
 }
