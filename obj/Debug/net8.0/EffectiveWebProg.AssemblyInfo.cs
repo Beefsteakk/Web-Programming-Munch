5a4d76d9fba4d50a65f76dc499192e7923a54fe5
Ayers <fongayers12@gmail.com>
2024-06-11 13:05:02 +0800

    merged with darren

--- obj/Debug/net8.0/EffectiveWebProg.AssemblyInfo.cs
+++ obj/Debug/net8.0/EffectiveWebProg.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EffectiveWebProg")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ab5ba11abcf36b4e5617aff17781f0d1471290e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2914a399cad0bd38d5bc6b78b004dac1871032d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("EffectiveWebProg")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EffectiveWebProg")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
