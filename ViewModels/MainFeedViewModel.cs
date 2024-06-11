e440571cbe77436d3b327c616ab5a58cc690daa8
Anson-Quek <ansonquekmj@gmail.com>
2024-06-12 00:53:55 +0800

    Use PostViewModel instead of PostsModel.

--- ViewModels/MainFeedViewModel.cs
+++ ViewModels/MainFeedViewModel.cs
@@ -4,7 +4,7 @@ namespace EffectiveWebProg.ViewModels
 {
     public class MainFeedViewModel
     {
-        required public List<PostsModel> PostLists { get; set; }
+        required public List<PostViewModel> PostLists { get; set; }
         public PostsModel? NewPost { get; set; }
         public CommentsModel? NewComment { get; set; }
     }
