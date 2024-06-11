617ab6178f055a74f0866736e325f878694e43d3
Anson-Quek <ansonquekmj@gmail.com>
2024-06-11 21:26:50 +0800

    Made some attributes optional.

--- ViewModels/MainFeedViewModel.cs
+++ ViewModels/MainFeedViewModel.cs
@@ -5,6 +5,7 @@ namespace EffectiveWebProg.ViewModels
     public class MainFeedViewModel
     {
         required public List<PostsModel> PostLists { get; set; }
-        public PostsModel NewPost { get; set; }
+        public PostsModel? NewPost { get; set; }
+        public CommentsModel? NewComment { get; set; }
     }
 }
