a7b7478afe94936f7743481e08efbcc4407e367a
Anson-Quek <ansonquekmj@gmail.com>
2024-06-11 21:28:17 +0800

    Added comment functionality to the controller.

--- Controllers/PostsController.cs
+++ Controllers/PostsController.cs
@@ -15,16 +15,27 @@ public class PostsController : Controller
         _db = db;
     }
 
+    private void CompletePostObject(PostsModel post) {
+        if (post.UserID != null) {
+            post.User = _db.Users.Find(post.UserID);
+        } else if (post.RestID != null) {
+            post.Restaurant = _db.Restaurants.Find(post.RestID);
+        }
+    }
     private async Task<List<PostsModel>> GetAllPostsAsync() {
         var posts = await _db.Posts.OrderByDescending(p => p.PostCreatedAt).ToListAsync();
         foreach (var post in posts) {
-            if (post.UserID != null) {
-                post.User = await _db.Users.FindAsync(post.UserID);
-            } else if (post.RestID != null) {
-                post.Restaurant = await _db.Restaurants.FindAsync(post.RestID);
-            }
+            CompletePostObject(post);
         }
         return posts;
+    }
+
+    private async Task<List<CommentsModel>> GetSpecificPostCommentsAsync(Guid postID) {
+        var comments = await _db.Comments
+            .Where(c => c.PostID == postID)
+            .ToListAsync();
+
+        return comments;
     }
 
     public async Task<IActionResult> Index() {
@@ -38,6 +49,13 @@ public class PostsController : Controller
     [HttpPost]
     public async Task<IActionResult> CreatePosts(PostsModel post) {
         await _db.Posts.AddAsync(post);
+        await _db.SaveChangesAsync();
+        return RedirectToAction("");
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddComment(CommentsModel comment) {
+        await _db.Comments.AddAsync(comment);
         await _db.SaveChangesAsync();
         return RedirectToAction("");
     }
@@ -60,12 +78,26 @@ public class PostsController : Controller
     }
 
     [HttpPost]
-    public JsonResult GetInfo(int id)
+    async public Task<JsonResult> GetInfo(string id)
     {
+        var post = await _db.Posts.FindAsync(Guid.Parse(id));
         List<string> ImageUrl = new List<string>();
         ImageUrl.Add("/assets/images/photo-1556008531-57e6eefc7be4.jpeg");
         ImageUrl.Add("/assets/images/photo-1557684387-08927d28c72a.jpeg");
         ImageUrl.Add("/assets/images/photo-1526016650454-68a6f488910a.jpeg");
-        return Json(new {imageUrl = ImageUrl, info = $"Information for div {id}"});
+        return Json(new {imageUrl = ImageUrl, title = $"{post.PostTitle}", message = $"{post.PostContent}"});
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> SpecificPost(Guid id) {
+        Console.WriteLine(id.ToString());
+        var post = await _db.Posts.FirstOrDefaultAsync(p => p.PostID == id);
+        var comments = await GetSpecificPostCommentsAsync(id);
+        if (post == null) {
+            return NotFound();
+        }
+        CompletePostObject(post);
+        var model = new IndividualPostViewModel{Post=post, CommentsList=comments};
+        return View(model);
     }
 }
