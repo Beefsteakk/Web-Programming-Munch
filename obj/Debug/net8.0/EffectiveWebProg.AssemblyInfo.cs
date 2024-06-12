ce65d19656aa514620cfcc229da0c360164bb87b
Junjie14321 <junjie1432@gmail.com>
2024-06-12 23:07:42 +0800

    hieiehi

--- obj/Debug/net8.0/EffectiveWebProg.AssemblyInfo.cs
+++ obj/Debug/net8.0/EffectiveWebProg.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EffectiveWebProg")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f6aecee36e9c044e153ed597c5144e50d5124cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+516f3a86bafd4ad9d449ed7a6f05b7fa66197df6")]
 [assembly: System.Reflection.AssemblyProductAttribute("EffectiveWebProg")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EffectiveWebProg")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
