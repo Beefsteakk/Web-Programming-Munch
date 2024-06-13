0c4c54bb5b3f96ead98e752c7d53986d0751f54b
Deva33369 <kumar.devadharshini@gmail.com>
2024-06-13 13:14:10 +0800

    lsods

--- obj/Debug/net8.0/EffectiveWebProg.AssemblyInfo.cs
+++ obj/Debug/net8.0/EffectiveWebProg.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EffectiveWebProg")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2980b2206b0e58d33222db3030c09f9e3078db94")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e17f3e2923f2bbad034c5028889a6697a05fc8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("EffectiveWebProg")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EffectiveWebProg")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
