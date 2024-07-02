7abd0b483d08ab94095508cb9fe16d3f6479f2cf
Anson-Quek <ansonquekmj@gmail.com>
2024-07-03 02:06:21 +0800

    Start dynamically generating profile page.

--- Controllers/ProfileController.cs
+++ Controllers/ProfileController.cs
@@ -1,14 +1,23 @@
+using EffectiveWebProg.Data;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 
 namespace EffectiveWebProg.Controllers
 {
-    public class ProfileController : Controller
+    public class ProfileController(ApplicationDbContext db) : BaseController
     {
-        public IActionResult Index()
-        {
+        private readonly ApplicationDbContext _db = db;
 
-            return View();
+        public async Task<IActionResult> Index()
+        {
+            if (HttpContext.Session.GetString("SSUserType") != "User") return Forbid();
+            var sessionID = Guid.Parse(HttpContext.Session.GetString("SSID") ?? "");
+            var user = await _db.Users.FirstAsync(u => u.UserID == sessionID);
+            if (user == null) {
+                Response.StatusCode = 500;
+                return Content("Internal Server Error: An unknown issue was encountered, please retry.");
+            }
+            return View(user);
         }
     }
 }
