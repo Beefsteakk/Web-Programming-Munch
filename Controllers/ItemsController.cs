e30adb4591e4c3c653d124b504ebad9db23f8083
addsw <addsw@hotmail.co.uk>
2024-07-04 15:20:06 +0800

    item done

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -1,11 +1,10 @@
+using EffectiveWebProg.Data;
+using EffectiveWebProg.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using System;
 using System.Linq;
 using System.Threading.Tasks;
-using EffectiveWebProg.Models;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using EffectiveWebProg.Data;
 
 namespace EffectiveWebProg.Controllers
 {
@@ -23,28 +22,6 @@ namespace EffectiveWebProg.Controllers
         {
             var items = await _db.Items.Include(i => i.ItemCat).ToListAsync();
             return View(items);
-        }
-
-        // GET: Items/Details/5
-        public async Task<IActionResult> Details(Guid? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var item = await _db.Items
-                .Include(i => i.ItemCat)
-                .FirstOrDefaultAsync(m => m.ItemID == id);
-
-            if (item == null)
-            {
-                return NotFound();
-            }
-
-            ViewBag.CatName = item.ItemCat.CatName;
-
-            return PartialView("_ViewModal", item);
         }
 
         // GET: Items/Create
@@ -78,17 +55,13 @@ namespace EffectiveWebProg.Controllers
                 return NotFound();
             }
 
-            var itemsModel = await _db.Items.Include(i => i.ItemCat).FirstOrDefaultAsync(i => i.ItemID == id);
+            var itemsModel = await _db.Items.FindAsync(id);
             if (itemsModel == null)
             {
                 return NotFound();
             }
-
-            var itemCat = await _db.ItemCat.FindAsync(itemsModel.CatID);
-            ViewBag.CatName = itemCat?.CatName;
-
             ViewData["CatID"] = new SelectList(_db.ItemCat, "CatID", "CatName", itemsModel.CatID);
-            return PartialView("_EditModal", itemsModel);
+            return View(itemsModel);
         }
 
         // POST: Items/Edit/5
@@ -141,9 +114,7 @@ namespace EffectiveWebProg.Controllers
                 return NotFound();
             }
 
-            ViewBag.CatName = itemsModel.ItemCat.CatName;
-
-            return PartialView("_DeleteModal", itemsModel);
+            return View(itemsModel);
         }
 
         // POST: Items/Delete/5
