656ba953f65f2be25f228d6b76bd957b2e71f630
Euynixxx <xy.lin03@gmail.com>
2024-07-23 19:23:44 +0800

    add timesheet filter

--- wwwroot/js/timesheet.js
+++ wwwroot/js/timesheet.js
@@ -158,4 +158,50 @@ function updateEditShiftTimes() {
     }
 }
 
+function sortTable() {
+    var table, rows, switching, i, x, y, shouldSwitch;
+    table = document.getElementById("employeeTable");
+    switching = true;
+    while (switching) {
+        switching = false;
+        rows = table.rows;
+        for (i = 1; i < (rows.length - 1); i++) {
+            shouldSwitch = false;
+            x = rows[i].getElementsByTagName("TD")[0];
+            y = rows[i + 1].getElementsByTagName("TD")[0];
+            if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                shouldSwitch = true;
+                break;
+            }
+        }
+        if (shouldSwitch) {
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+        }
+    }
+}
+
+function filterTable() {
+    var filter = document.getElementById("shiftFilter").value;
+    var table = document.getElementById("employeeTable");
+    var tr = table.getElementsByTagName("tr");
+
+    for (var i = 1; i < tr.length; i++) {
+        var td = tr[i].getElementsByTagName("td");
+        var showRow = false;
+
+        for (var j = 1; j < td.length; j++) {
+            var span = td[j].getElementsByTagName("span")[0];
+            if (span) {
+                if (filter === "all" || span.textContent === filter) {
+                    showRow = true;
+                    break;
+                }
+            }
+        }
+
+        tr[i].style.display = showRow ? "" : "none";
+    }
+}
+
 console.log("timesheet.js loaded");
